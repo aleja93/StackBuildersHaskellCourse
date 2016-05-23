b80c9a181a62114aff80c3f5c1e44b422ea98833
aleja93 <aledennis.93@gmail.com>
2016-05-23 12:11:01 -0500

    Auxiliar file

--- homework02/src/hh.hs
+++ homework02/src/hh.hs
@@ -51,12 +51,16 @@ parse x = [parseMessage(l)]++parse(rest)
           rest= getRestMessage ( l++" ") x
 
 -- "I 1578 Which brought them back again to the beginning of the conversation.\nW 3955 uhcd nosted)\nI 5071 #72"
+-- "I 3466 pci_hcd beed VRAM=2)
+-- "I 2788 from?'"
+-- "I 1108 had vanished completely."
+-- "W 159 [drm/ing locouchabet 1000000 pags: bus not D32 us=io2/ing 133"
 
 m1=parseMessage "I 15 Message1"
 m2=parseMessage "I 20 Message2"
 m3=parseMessage "I 30 Message3"
 m4=parseMessage "I 40 Message4"
-m5=parseMessage "I 20 Message5"
+m5=parseMessage "I 50 Message5"
 u=parseMessage "This is not the formatt"
 t1=Leaf
 t2=Node Leaf m2 Leaf
@@ -73,8 +77,38 @@ insert n@(LogMessage _ ts1 _) (Node Leaf p@(LogMessage _ ts2 _) mtr@(Node _ _ _)
   | ts2>ts1 = Node (Node Leaf n Leaf) p mtr
   | otherwise = Node Leaf p (insert n mtr)
 insert n@(LogMessage _ ts1 _) (Node mti@(Node _ _ _) p@(LogMessage _ ts2 _) mtr@(Node _ _ _))
-  | ts2>ts1 = Node (insert n mti) p Leaf
-  | otherwise = Node Leaf p (insert n mtr)
+  | ts2>ts1 = Node (insert n mti) p mtr
+  | otherwise = Node mti p (insert n mtr)
 insert n@(LogMessage _ ts1 _) (Node Leaf p@(LogMessage _ ts2 _) Leaf)
   | ts2>ts1 = Node (Node Leaf n Leaf) p Leaf
   | otherwise = Node Leaf p (Node Leaf n Leaf)
+
+listLogMessage=parse "I 15 Message1\nI 20 Message2\nI 30 Message3\nI 40 Message4\n 50 Message5"
+listLogMessage0=parse ""
+listLogMessage1=parse "I 15 Message1"
+listLogMessage2=parse "I 30 Message30\nI 20 Message2\nI 50 Message3\nI 40 Message4\nW 30 Message5"
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build [x] = insert x Leaf
+build (x:xs) = insert x (build xs)
+
+
+treeM= build listLogMessage2
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf= []
+inOrder (Node Leaf m Leaf) = [m]
+inOrder (Node mtl@(Node _ _ _) m Leaf) = inOrder(mtl)++[m]
+inOrder (Node Leaf m mtr@(Node _ _ _)) = [m]++inOrder(mtr)
+inOrder (Node mtl@(Node _ _ _) m mtr@(Node _ _ _)) = inOrder(mtl)++[m]++inOrder(mtr)
+
+messages= "I 20 Which brought \nW 60 it is \nI 55 Today \nThis is not\nI 11 had vanished completely.\nW 61 a great day"
+listM=parse messages
+listMinOrder= inOrder (build listM)
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong []=[]
+whatWentWrong ( (LogMessage _ ts m) :xs)
+  | ts>50 = [m]++ whatWentWrong( inOrder (build xs))
+  | otherwise = whatWentWrong(inOrder (build xs))
