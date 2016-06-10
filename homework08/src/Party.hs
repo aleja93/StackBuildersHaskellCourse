fb319712e55db77ad3d20aa51ce3bd9ee4472fb1
aleja93 <aledennis.93@gmail.com>
2016-06-10 17:57:59 -0500

    Exercise 8.1 & 8.3

--- homework08/src/Party.hs
+++ homework08/src/Party.hs
@@ -15,24 +15,51 @@ import Data.Tree
 ----------------------------------------------------------------------
 
 glCons :: Employee -> GuestList -> GuestList
-glCons = undefined
+glCons e@Emp{empFun=f} (GL l tf) = GL (e:l) (tf+f)
+
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun = undefined
+moreFun l1@(GL _ f1) l2@(GL _ f2)
+  | f1>f2     = l1
+  | otherwise = l2
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+  GL l1 f1 `mappend` GL l2 f2 = GL (l1++l2) (f1+f2)
 
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
+treeFold :: (a -> b -> b) -> b -> Tree a -> [b]
+treeFold f z (Node x []) = [f x z]
+treeFold f z (Node x ns) = [f x y | n <- ns, y <- treeFold f z n]
+
+
+--treeFold :: b -> (b -> a -> b -> b) -> Tree a -> b
+--treeFold e _ Empty        = e
+--treeFold e f (Node l x r) = f (treeFold e f l) x (treeFold e f r)
+
+--treeSum :: Tree Integer -> Integer
+--treeSum Empty     = 0
+--treeSum (Node l x r)  = x + treeSum l + treeSum r
 
 
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
 
+compareTwoList :: (GuestList, GuestList) -> GuestList
+compareTwoList (a, b) = moreFun a b
+
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel = undefined
+nextLevel _ []     = (mempty, mempty)
+nextLevel e (x:xs) = (final , (glCons e final))
+   where
+     best  = compareTwoList x
+     other = nextLevel e xs
+     final = best `mappend` (compareTwoList other)
 
 
 ----------------------------------------------------------------------
